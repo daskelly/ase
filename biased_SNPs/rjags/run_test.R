039eb2e18cc2e055cdc8d26aad0fa7f0b1183447
Dan Skelly <das78@duke.edu>
2016-06-14 22:53:34 -0400

    correct error in dat.snpsp

--- biased_SNPs/rjags/run_test.R
+++ biased_SNPs/rjags/run_test.R
@@ -11,7 +11,7 @@ mat$p <- apply(mat, 1, function(row) binom.test(row[1], row[2])$p.value)
 # according to the binomial exact test. This is as per instructions in
 # http://doingbayesiandataanalysis.blogspot.com/2012/06/mixture-of\
 # -normal-distributions.html, for implementing a mixture model in JAGS
-isNull <- rep(NA, dat$n.snps)
+isNull <- rep(NA, n.snps)
 isNull[which.min(mat$p)] <- FALSE
 isNull[which.max(mat$p)] <- TRUE
 datlist <- list(nsnps=dat$n.snps, Y=dat$Y, N=dat$N)
