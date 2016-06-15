bc3508f0aeafdea871f3de31b2c1c95e1f5a4cf7
Dan Skelly <das78@duke.edu>
2016-06-15 13:36:53 -0400

    add option to specify number of SNPs

--- biased_SNPs/simulate_data.R
+++ biased_SNPs/simulate_data.R
@@ -2,18 +2,18 @@
 # is giving us reasonable results.
 # Note that we are simulating data according to the exact 
 # statistical model we implemented, so this should be an easy task.
-simulate.data <- function(seed=1234) {
+simulate.data <- function(seed=12345, n.snps=10000) {
     set.seed(seed)
-    n.snps <- 10000
     pi0 <- 0.95
     alpha <- rlnorm(1, 4.3, 1.8)
-    delta <- runif(1)
-    epsilon <- runif(1)
+    delta <- 0.1
+    epsilon <- 0.2
     avg.coverage <- 50
     N <- rpois(n.snps, lambda=avg.coverage)
     # generate beta-binomially distributed values:
     isNull <- runif(n.snps) < pi0
     Y <- isNull*rbinom(n.snps, N, rbeta(n.snps, alpha, alpha)) + 
         (1 - isNull)*rbinom(n.snps, N, rbeta(n.snps, delta, epsilon))
-    list(Y=Y, N=N, isNull=isNull, alpha=alpha, delta=delta, epsilon=epsilon)
+    list(Y=Y, N=N, isNull=isNull, alpha=alpha, delta=delta, epsilon=epsilon,
+        pi0=pi0, n.snps=n.snps)
 }
